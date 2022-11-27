7a6358f6e9441046790cb411dc3292a820361b91
viraj124 <virajm72@gmail.com>
2022-11-27 21:57:08 +0530

    feat: add deployment script for allocators

---
+++ contracts/scripts/DeployV1Allocator.s.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.6;
+
+import '../V1Allocator.sol';
+import 'forge-std/Script.sol';
+
+
+contract Deploy_V1_Allocator_On_Mainnet is Script {
+
+    IJBDirectory jbV3Directory = IJBDirectory(0x65572FB928b46f9aDB7cfe5A4c41226F636161ea);
+    V1Allocator v1Allocator;
+
+    function run() external {
+      vm.startBroadcast();
+
+      v1Allocator = new V1Allocator(jbV3Directory);
+    }
+}
+
+contract Deploy_V1_Allocator_On_Goerli is Script {
+
+    IJBDirectory jbV3Directory = IJBDirectory(0x8E05bcD2812E1449f0EC3aE24E2C395F533d9A99);
+    V1Allocator v1Allocator;
+
+    function run() external {
+      vm.startBroadcast();
+
+      v1Allocator = new V1Allocator(jbV3Directory);
+    }
+}
