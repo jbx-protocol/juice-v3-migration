ee8c107264b48fa20a97c2238c9cc081f2945bd5
mejango <mejango@protonmail.com>
2022-12-02 10:48:50 -0500

    smol

--- contracts/JBV3TokenDeployer.sol
+++ contracts/JBV3TokenDeployer.sol
@@ -6,8 +6,6 @@ import '@jbx-protocol-v2/contracts/interfaces/IJBProjects.sol';
 // since importing v3 & v2 contract together give compilation issues because of same contract names & setFor isn't available in the v2 interface so using this custom interface
 import './interfaces/IJBV3TokenStore.sol';
 import './JBV3Token.sol';
- 
-
 
 /** 
   @notice
@@ -36,63 +34,67 @@ contract JBV3TokenDeployer {
   */
   IJBProjects public immutable v3_v2_ProjectDirectory;
 
-
   /** 
     @notice
     The V3 token store.
   */
   IJBV3TokenStore public immutable tokenStore;
 
-
-    //*********************************************************************//
-    // -------------------------- constructor ---------------------------- //
-    //*********************************************************************//
-    /** 
+  //*********************************************************************//
+  // -------------------------- constructor ---------------------------- //
+  //*********************************************************************//
+  /** 
       @param _v3_v2_ProjectDirectory V3 & V2 project directory address.
       @param _v1ProjectDirectory V1 project directory address.
     */
-    constructor(IJBProjects _v3_v2_ProjectDirectory, IProjects _v1ProjectDirectory, IJBV3TokenStore _tokenStore) {
-      v3_v2_ProjectDirectory = _v3_v2_ProjectDirectory;
-      v1ProjectDirectory = _v1ProjectDirectory;
-      tokenStore = _tokenStore;
-    }
+  constructor(
+    IJBProjects _v3_v2_ProjectDirectory,
+    IProjects _v1ProjectDirectory,
+    IJBV3TokenStore _tokenStore
+  ) {
+    v3_v2_ProjectDirectory = _v3_v2_ProjectDirectory;
+    v1ProjectDirectory = _v1ProjectDirectory;
+    tokenStore = _tokenStore;
+  }
 
-
-    /**
+  /**
       @param _name The name of the token.
       @param _symbol The symbol that the token should be represented by.
       @param _projectId The ID of the project that this token should be exclusively used for. Send 0 to support any project.
       @param _v1TicketBooth V1 Token Booth Instance.
       @param _v2TokenStore V2 Token Store Instance.
-      @param _v2ProjectId V2 Project Id.
       @param _v1ProjectId V1 Project Id.
     */
-    function deploy(
-      string memory _name,
-      string memory _symbol,
-      uint256 _projectId,
-      ITicketBooth _v1TicketBooth,
-      IJBTokenStore _v2TokenStore,
-      uint128 _v2ProjectId,
-      uint128 _v1ProjectId
-      ) external { 
-          // only the project owner an deploy the token
-          if (_v1ProjectId != 0 && v1ProjectDirectory.ownerOf(_v1ProjectId) != msg.sender)
-            revert NOT_OWNER();
-        
-          if (_v2ProjectId != 0 && v3_v2_ProjectDirectory.ownerOf(_v2ProjectId) != msg.sender)
-            revert NOT_OWNER();
-        
-          if (_projectId != 0 && v3_v2_ProjectDirectory.ownerOf(_projectId) != msg.sender)
-            revert NOT_OWNER();
+  function deploy(
+    string memory _name,
+    string memory _symbol,
+    uint256 _projectId,
+    ITicketBooth _v1TicketBooth,
+    IJBTokenStore _v2TokenStore,
+    uint128 _v1ProjectId
+  ) external {
+    // only the project owner an deploy the token
+    if (_v1ProjectId != 0 && v1ProjectDirectory.ownerOf(_v1ProjectId) != msg.sender)
+      revert NOT_OWNER();
 
-          JBV3Token _v3Token = new JBV3Token(_name, _symbol, _projectId, _v1TicketBooth, _v2TokenStore, _v2ProjectId, _v1ProjectId);
-          // attachhing the token to the project
-          tokenStore.setFor(_projectId, _v3Token);
+    if (_projectId != 0 && v3_v2_ProjectDirectory.ownerOf(_projectId) != msg.sender)
+      revert NOT_OWNER();
 
-          // transferring the ownership to the project owner
-          _v3Token.transferOwnership(_projectId, msg.sender);
+    JBV3Token _v3Token = new JBV3Token(
+      _name,
+      _symbol,
+      _projectId,
+      _v1TicketBooth,
+      _v2TokenStore,
+      _v2ProjectId,
+      _v1ProjectId
+    );
+    // attachhing the token to the project
+    tokenStore.setFor(_projectId, _v3Token);
 
-          emit Deployed(_projectId, address(_v3Token), msg.sender);
-    }
+    // transferring the ownership to the project owner
+    _v3Token.transferOwnership(_projectId, address(tokenStore));
+
+    emit Deployed(_projectId, address(_v3Token), msg.sender);
+  }
 }
