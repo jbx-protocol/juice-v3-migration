c586d54a4297d7d3bf21f9a91ccab50cef972d19
drgorillamd <dr_gorilla@protonmail.com>
2022-12-14 10:59:38 +0100

    mainnet deployments + addresses + V3_Token Goerli

--- contracts/scripts/DeployJBV3TokenDeployer.sol
+++ contracts/scripts/DeployJBV3TokenDeployer.sol
@@ -19,3 +19,29 @@ contract Deploy_JB_V3_Token_Deployer_On_Mainnet is Script {
       console.log(address(v3TokenDeployer));
     }
 }
+
+  /**
+  * @notice for testnet purpose ONLY
+  */
+contract MockV1ProjectDirectory {
+    function ownerOf(uint256 _projectId) external view returns (address) {
+      _projectId;
+      return msg.sender;
+    }
+}
+
+contract Deploy_JB_V3_Token_Deployer_On_Goerli is Script {
+    
+    IProjects v1ProjectDirectory = IProjects(address(new MockV1ProjectDirectory()));
+
+    IJBProjects v3_v2_ProjectDirectory = IJBProjects(0x21263a042aFE4bAE34F08Bb318056C181bD96D3b);
+    IJBV3TokenStore tokenStore = IJBV3TokenStore(0x1246a50e3aDaF684Ac566f0c40816fF738F309B3);
+    JBV3TokenDeployer v3TokenDeployer;
+
+    function run() external {
+      vm.startBroadcast();
+
+      v3TokenDeployer = new JBV3TokenDeployer(v3_v2_ProjectDirectory, v1ProjectDirectory, tokenStore);
+      console.log(address(v3TokenDeployer));
+    }
+}
